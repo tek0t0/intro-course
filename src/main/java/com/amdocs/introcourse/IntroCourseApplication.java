ae0ea45eff0b875252723845ee038e4350711e1d
Stoyan Mitev <tekoto@abv.bg>
2021-06-26 19:15:27 +0300

    Initial commit

---
+++ src/main/java/com/amdocs/introcourse/IntroCourseApplication.java
@@ -0,0 +1,13 @@
+package com.amdocs.introcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amdocs/introcourse/IntroCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amdocs.introcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
